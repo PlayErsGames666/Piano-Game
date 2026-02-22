073b101d93a3e5020f7b4fc121700590d69862ee
PlayErsGames666 <playersgames.ru@gmail.com>
2026-02-22 20:11:36 +0500

    Deleting module keyboard and making optimization.

--- piano.py
+++ piano.py
@@ -1,9 +1,16 @@
-import keyboard
-import pygame 
+import pygame
+
 from pygame import mixer
 
+#Inicialization
+pygame.init()
 mixer.init()
 
+#Create window
+screen = pygame.display.set_mode((400, 200))
+pygame.display.set_caption("PlayErsGmaes Piano Game")
+
+#Downloading sounds from nearest directory
 do = mixer.Sound('../notes/do.wav')
 re = mixer.Sound('../notes/re.wav')
 mi = mixer.Sound('../notes/mi.wav')
@@ -12,19 +19,29 @@ sol = mixer.Sound('../notes/sol.wav')
 lja = mixer.Sound('../notes/lja.wav')
 si = mixer.Sound('../notes/si.wav')
 
+#Making keyboard letter functional
 keymap = {
-    'q': do,
-    'w': re,
-    'e': mi,
-    'r': fa,
-    't': sol,
-    'y': lja,
-    'u': si
+    pygame.K_q: do,
+    pygame.K_w: re,
+    pygame.K_e: mi,
+    pygame.K_r: fa,
+    pygame.K_t: sol,
+    pygame.K_y: lja,
+    pygame.K_u: si
 }
 
-for key, note in keymap.items():
-    keyboard.add_hotkey(key, note.play)
+#Variable for Main cycle
+running = True
 
-while True:
-     pass
+#Main cycle
+while running:
+    for event in pygame.event.get(): #Event handling
+        if event.type == pygame.QUIT: #X closing the window and game
+            running = False
 
+        if event.type == pygame.KEYDOWN: #Keyboard handling
+            if event.key in keymap: #Check vocabulary
+                keymap[event.key].play()
+
+#End cycle
+pygame.quit()
